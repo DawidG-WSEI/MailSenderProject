95b8ba28778c506289bd9035a577c8f7be28d95c
DawidG-WSEI <dawid.grzymkowski@microsoft.wsei.edu.pl>
2026-06-19 17:57:45 +0200

    Delete old Services directory

--- Services/IMailProvider.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MailSender.Services;
-
-public interface IMailProvider
-{
-    //Używamy Task, ponieważ wysyłanie maili przez sieć to operacja asynchroniczna
-    Task SendEmailAsync(string to, string subject, string body);    
-}
